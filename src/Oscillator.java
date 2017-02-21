724600239de95f76144a5ffaec57d65c111180d0
carlosarli <carlosarli@137.44.227.151>
2017-02-21 17:34:11 +0000

    Started Oscillator

--- src/Oscillator.java
+++ src/Oscillator.java
@@ -1,4 +1,17 @@
+import org.jaudiolibs.jnajack.examples.SineAudioSource;
+
+import net.beadsproject.beads.ugens.WavePlayer;
 
 public class Oscillator {
+	
+	Settings settings = new Settings();
 
+	WavePlayer sine,square,saw,triangle,noise;
+	
+	sine = new WavePlayer(ac,settings.getOsc1Freq(),Buffer.SINE);
+	square = new WavePlayer(ac,settings.getOsc1Freq(),Buffer.SQUARE);
+	saw = new WavePlayer(ac,settings.getOsc1Freq(),Buffer.SAW);
+	triangle = new WavePlayer(ac,settings.getOsc1Freq(),Buffer.TRIANGLE);
+	noise = new WavePlayer(ac,settings.getOsc1Freq(),Buffer.NOISE);
+	
 }
