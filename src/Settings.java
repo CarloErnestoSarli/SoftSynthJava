fb04e70585995926b1cb8cb44a2c7898d85c6f15
carlosarli <carlosarli@137.44.227.151>
2017-02-21 12:32:32 +0000

    made ac static
    started working on settings class

--- src/Settings.java
+++ src/Settings.java
@@ -1,4 +1,34 @@
+import net.beadsproject.beads.*;
+
+import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.ugens.WavePlayer;
 
 public class Settings {
-
+	
+	static AudioContext ac = new AudioContext();
+	
+	private double m_Osc1Freq;
+	private double m_Osc2Freq;
+	
+	
+	
+	public void setOsc1Freq(double freq){
+		m_Osc1Freq = freq;
+	}
+	
+	public void setOsc2Freq(double freq){
+		m_Osc1Freq = freq;
+	}
+	
+	public double getOsc1Freq(){
+		
+		return m_Osc1Freq;
+	}
+	
+	public double getOsc2Freq(){
+		
+		return m_Osc2Freq;
+	}
+	
 }
